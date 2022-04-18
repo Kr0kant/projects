49ab1975c243a78afb9bd7a0c21da105616c2709
Kr0kant <ovbarantsev@gmail.com>
2022-04-18 21:41:47 +0300

    ц

---
+++ web/web.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def Go():
+    if request.method == 'GET':
+        name_param = request.args.get('name')
+
+    if name_param is None:
+        name_param = "Анонимус"
+
+    return render_template(
+        'mainpage.html',
+        name=name_param,
+        method=request.method
+    )
+
+@app.context_processor
+def inject_globals():
+    return {
+        "videos": ["https://www.youtube.com/embed/jsBnFHdsSjc", "https://www.youtube.com/embed/zGhqZGAbIqY", "https://www.youtube.com/embed/vjWvHO6hZgA", "https://www.youtube.com/embed/lOgL9K0lpbc", "https://www.youtube.com/embed/drd3TIk-4QE"
+        ]
+    }
+
+if __name__ == "__main__":
+    app.run()
